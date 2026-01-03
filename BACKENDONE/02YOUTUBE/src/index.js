1d3738f61394b514fb646e9e5a4a97a9b7ef448c
connectoshivam <connecttoshivam1@gmail.com>
2026-01-03 23:36:49 +0530

    add the prettier files

--- BACKENDONE/02YOUTUBE/src/index.js
+++ BACKENDONE/02YOUTUBE/src/index.js
@@ -1,1 +1,30 @@
-console.log("radhe radhe")
+// console.log("radhe radhe")
+
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+
+dotenv.config({
+    path : './env'
+})
+
+connectDB()
+
+// const app = express()
+
+// ;(async()=>{
+//     try{
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error",(error)=>{
+//             console.log("eroor: ",error);
+//             throw error
+//         })
+
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`app is listening http://localhost:${process.env.PORT}`)
+//         })
+
+//     }catch(error){
+//         console.error("something wrong:",error);
+//         throw error
+//     }
+// })()
