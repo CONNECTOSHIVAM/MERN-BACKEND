882e1277667da4de89cd7cecc1999785567dd640
connectoshivam <connecttoshivam1@gmail.com>
2026-01-08 14:39:26 +0530

    mongoDB connection setup

---
+++ BACKEND-CRUD/CRUDApp/src/config/database.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const ConnectDB = async ()=>{
+
+    try {
+        const connectionInstance = await mongoose.connect(process.env.MONGODB_URI);
+        console.log(`MongoDB is connection successfull : ${connectionInstance.connection.host}`);
+        
+    } catch (error) {
+        console.log(`MongoDB connetion failed. ${error}`)
+        process.exit(1);
+    }
+}
+
+export default ConnectDB;
---
+++ BACKEND-CRUD/CRUDApp/src/index.js
@@ -0,0 +1,28 @@
+import dotenv from "dotenv"
+import ConnectDB from "./config/database.js"
+import app from "./app.js"
+
+dotenv.config({
+    path: './.env'
+})
+
+const startServer = async ()=>{
+
+   try {
+      await ConnectDB();
+
+      app.on("error",(error)=>{
+        console.log("server connection issue:",error)
+        throw error
+      })
+
+      app.listen(process.env.PORT || 8000,()=>{
+         console.log(`Server is listening at port : ${process.env.PORT}`);
+      })
+    
+   } catch (error) {
+     console.log(`MongoDB connection nahi hua Ji.-----> ${error}`)
+     process.exit(1);
+   }
+}
+startServer();
