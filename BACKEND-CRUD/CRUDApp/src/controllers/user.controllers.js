4fa54db8cb66a6ecd610dfc197c8bb223972680c
connectoshivam <connecttoshivam1@gmail.com>
2026-01-08 15:48:11 +0530

    add the controllers files.

--- BACKEND-CRUD/CRUDApp/src/controllers/user.controllers.js
+++ BACKEND-CRUD/CRUDApp/src/controllers/user.controllers.js
@@ -7,10 +7,32 @@ const registerUser = async (req,res)=>{
         
         //basic validation
         if(!username ||!email ||!password){
-            return res.status
+            return res.status(400).json({message: "All fields are must required."})
         }
+
+        //check if user already exist in database.
+        const existUser = await User.findOne({email: email.toLowerCase()}) 
+
+        if(existUser){
+            return res.status(400).json({messgae: "User is already exist."})
+        }
+
+        //create a user
+        const user = await User.create({
+            username,
+            email,
+            password,
+            loggenIn: false
+        })
+
+        res.status(201).json({
+            message: "user is registered successfully.",
+            user: {id: user._id,username: user.username, email:user.email}
+        })
         
     } catch (error) {
         return res.status(500).json("Server error aayya my dear!")
     }
 }
+
+export {registerUser}
