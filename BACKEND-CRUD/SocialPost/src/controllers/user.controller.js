52dd423d2b0ade47e267fc35c9067039ad199a89
connectoshivam <connecttoshivam1@gmail.com>
2026-01-09 23:54:51 +0530

    add the controller files

--- BACKEND-CRUD/SocialPost/src/controllers/user.controller.js
+++ BACKEND-CRUD/SocialPost/src/controllers/user.controller.js
@@ -6,9 +6,19 @@ const registerUser = async (req,res) => {
         
         const {username,email,password} = req.body;
 
+        //basic validations
         if(!username ||!email ||!password){
             return res.status(400).json({message: "All fields are must required."})
         }
+
+        //check if user is already exists.
+        const existUser = await User.findOne({email: email.toLowerCase()})
+
+        if(existUser){
+            return res.status(400).json({message: "user is already exists."})
+        }
+
+
     } catch (error) {
         
     }
