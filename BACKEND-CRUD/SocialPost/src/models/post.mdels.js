7ac5e4f1bcd7944cfc5e439f782130e4b9457b0a
connectoshivam <connecttoshivam1@gmail.com>
2026-01-10 14:41:23 +0530

    add the post functionlaties

---
+++ BACKEND-CRUD/SocialPost/src/models/post.mdels.js
@@ -0,0 +1,22 @@
+import mongoose, {Schema} from "mongoose";
+
+const postSchema = new Schema({
+    name:{
+        type: String,
+        required: true,
+        trim: true,
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true 
+    },
+    age: {
+        type: Number,
+        trim: true,
+        min: 1,
+        max: 10000
+    }
+},{timestamps: true})
+
+export const Post = mongoose.model("Post",postSchema)
