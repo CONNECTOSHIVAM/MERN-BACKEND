37c5ead1698885eb0374a663d5078954f514a3de
connectoshivam <connecttoshivam1@gmail.com>
2026-01-10 13:52:07 +0530

    add the optimizing route file

--- BACKEND-CRUD/SocialPost/src/app.js
+++ BACKEND-CRUD/SocialPost/src/app.js
@@ -1,5 +1,13 @@
 import express from "express"
 
 const app = express();
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+//import the routes.
+import userRouter from './routes/user.routes.js'
+
+//route declarations...
+app.use('/api/v1/users',userRouter)
 
 export default app;
