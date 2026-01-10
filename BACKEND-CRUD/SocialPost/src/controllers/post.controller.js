9adf472fc7f7c84d6744dd70c8c41863c067a5f5
connectoshivam <connecttoshivam1@gmail.com>
2026-01-10 15:34:10 +0530

    add the post creation functionlaties

--- BACKEND-CRUD/SocialPost/src/controllers/post.controller.js
+++ BACKEND-CRUD/SocialPost/src/controllers/post.controller.js
@@ -1,1 +1,29 @@
 import {Post} from "../models/user.models.js"
+
+const createPost = async (req,res) => {
+
+    try {
+
+        const {name, description, age} = req.body;
+
+        if(!name || !description || !post) return res.status(400).json
+        ({
+            message: "All feel are must required."
+        })
+
+        const post = await Post.create({name,description,age})
+
+        res.status(200).json({
+            message: "Post Created successfully.",
+            post
+        })
+    
+        
+    } catch (error) {
+        console.error("Post Creation error: ",error);
+        return res.status(500).json({message: "Internal server error."+error.message})
+    }
+}
+
+
+export {createPost};
