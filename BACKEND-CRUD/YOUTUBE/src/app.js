2f79a4e371701333085f981141b79381e5c77f7e
connectoshivam <connecttoshivam1@gmail.com>
2026-01-13 17:35:05 +0530

    update the file fix the bug

--- BACKEND-CRUD/YOUTUBE/src/app.js
+++ BACKEND-CRUD/YOUTUBE/src/app.js
@@ -13,4 +13,13 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"));
 app.use(cookieParser())
 
+// import the routes
+
+import userRouter from "./routes/user.routes.js"
+
+
+// route declarations.
+
+app.use('/api/v1/users',userRouter)
+
 export {app}
