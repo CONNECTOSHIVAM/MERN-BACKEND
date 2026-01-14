9a69c2ef7fc584e1d8dee4298379b5fbf9cd1631
connectoshivam <connecttoshivam1@gmail.com>
2026-01-14 18:22:24 +0530

    intilal code setup

--- BACKEND-CRUD/YOUTUBE/src/controller/user.controller.js
+++ BACKEND-CRUD/YOUTUBE/src/controller/user.controller.js
@@ -2,9 +2,20 @@ import {asyncHandler} from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler( async(req, res)=>{
 
-    res.status(200).json({
-        message: "ok "
-    })
+    // res.status(200).json({
+    //     message: "Radhe radhe "
+    // })
+
+    // Algo:
+    //get user details from fronend.
+    // validation - not empty
+    // check if user already exists: username, email
+    // check for Images and avtar
+    // upload them to clodinary
+    // create a usr object - create entry in db.
+    // remove password and refresh token field from response.
+    // check for user creation.
+    // return res
 })
 
 export {registerUser}
