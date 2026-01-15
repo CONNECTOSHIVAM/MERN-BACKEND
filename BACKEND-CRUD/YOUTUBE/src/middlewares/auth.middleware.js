adcb4fe70f2c4ac72881beeb6f2053e33b7b0a92
connectoshivam <connecttoshivam1@gmail.com>
2026-01-15 15:20:36 +0530

    write auth middleware functionlaties

--- BACKEND-CRUD/YOUTUBE/src/middlewares/auth.middleware.js
+++ BACKEND-CRUD/YOUTUBE/src/middlewares/auth.middleware.js
@@ -1,27 +1,27 @@
-// import { ApiError } from "../utils/ApiError.js";
-// import { asyncHandler } from "../utils/asyncHandler.js";
-// import jwt from "jsonwebtoken"
-// import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken"
+import { User } from "../models/user.model.js";
 
 
-// export const verifyJWT = asyncHandler(async(req,res,next)=>{
+export const verifyJWT = asyncHandler(async(req,res,next)=>{
    
-//     try {
-//         const token = req.cookies?.accessToken() || req.header("Authorization")?.replace("Bearer","")
+    try {
+        const token = req.cookies?.accessToken() || req.header("Authorization")?.replace("Bearer","")
     
-//         if(!token) throw new ApiError(401,"Unauthorized request")
+        if(!token) throw new ApiError(401,"Unauthorized request")
     
-//         const decodeToken = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+        const decodeToken = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
     
-//         const user = await User.findById(decodeToken?._id).select("-password -refreshToken")
+        const user = await User.findById(decodeToken?._id).select("-password -refreshToken")
     
-//         if(!user){
-//             throw new ApiError(401, "Invalid Access Token")
-//         }
+        if(!user){
+            throw new ApiError(401, "Invalid Access Token")
+        }
     
-//         req.user = user;
-//         next()
-//     } catch (error) {
-//         throw new ApiError(401, error?.message || "Invalid access token")
-//     }
-// })
+        req.user = user;
+        next()
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid access token")
+    }
+})
