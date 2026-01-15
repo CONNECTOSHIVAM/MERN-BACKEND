2a5b9dd09ca1db6b80b9952abbe7b2904588d5d9
connectoshivam <connecttoshivam1@gmail.com>
2026-01-15 08:22:45 +0530

    update the files

--- BACKEND-CRUD/YOUTUBE/src/controller/user.controller.js
+++ BACKEND-CRUD/YOUTUBE/src/controller/user.controller.js
@@ -1,4 +1,8 @@
+import { ApiError } from "../utils/ApiError.js";
 import {asyncHandler} from "../utils/asyncHandler.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler( async(req, res)=>{
 
@@ -24,8 +28,52 @@ const registerUser = asyncHandler( async(req, res)=>{
             field?.trim() === ""
         })
     ){
-        throw new A
+        throw new ApiError(400,"All fields are required.")
     }
+
+    const existedUser = User.findOne({
+        $or: [{username},{email}]
+    })
+
+    if(existedUser){
+        throw new ApiError(409,"Username and email is already exist.")
+    }
+
+    const avatarLoacalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path
+
+    if(!avatarLoacalPath){
+        throw new ApiError(400,"avatar file is must required.")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLoacalPath);
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+    if(!avatar){
+        throw new ApiError(400, "Avatar file is required.")
+    }
+
+    const user = await User.create({
+        fullName,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        email,
+        password,
+        username: username.toLowerCase()
+    })
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if(!createdUser){
+        throw new ApiError(500, "Server error in registering user.")
+    }
+
+    return res.status(201).json(
+        // new ApiResponse(200,createdUser,"User registered successfully.")
+    )
+
 })
 
 export {registerUser}
--- BACKEND-CRUD/YOUTUBE/src/models/user.model.js
+++ BACKEND-CRUD/YOUTUBE/src/models/user.model.js
@@ -39,7 +39,7 @@ const userSchema = new Schema({
        type: String,
        required: [true, "password is required."]
     },
-    avtar:{
+    avatar:{
         type: String, // cloudnaryurl
         required: true
     },
