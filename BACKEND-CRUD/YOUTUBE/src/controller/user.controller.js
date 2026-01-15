8448fc61bca712df43407cb7608c48a58326b2e7
connectoshivam <connecttoshivam1@gmail.com>
2026-01-15 15:16:59 +0530

    update the user controller files

--- BACKEND-CRUD/YOUTUBE/src/controller/user.controller.js
+++ BACKEND-CRUD/YOUTUBE/src/controller/user.controller.js
@@ -153,7 +153,25 @@ const loginUser = asyncHandler( async(req, res)=>{
 
 const logoutUser = asyncHandler( async(req,res)=>{
 
-    const {username , email} = req.body
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res.status(200).clearCookie("accessToken", options).clearCookie
+    ("refreshToken",options).json(new ApiResponse(200,{},"User logged out successfully."))
 })
 
-export {registerUser}
+export {registerUser, loginUser, logoutUser}
