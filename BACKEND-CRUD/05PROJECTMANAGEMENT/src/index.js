349a3ca813dd8b3c8d91a5f6033f897b9b6f5e9c
connectoshivam <connecttoshivam1@gmail.com>
2026-01-16 20:11:27 +0530

    create a express server

--- BACKEND-CRUD/05PROJECTMANAGEMENT/src/index.js
+++ BACKEND-CRUD/05PROJECTMANAGEMENT/src/index.js
@@ -1,1 +1,15 @@
-console.log("radhe radhe");
+// console.log("radhe radhe");
+import dotenv from "dotenv"
+import app from "./app.js"
+
+
+dotenv.config({
+    path: "./.env",
+})
+
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log(`app are listening at http://localhost:${port}`);
+})
