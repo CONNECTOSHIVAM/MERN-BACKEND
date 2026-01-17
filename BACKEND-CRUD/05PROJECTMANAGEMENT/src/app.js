a5f8bfebf666644bb32a3ab59546a381f2c35c28
connectoshivam <connecttoshivam1@gmail.com>
2026-01-17 08:48:16 +0530

    setup the cors cinfigurations

--- BACKEND-CRUD/05PROJECTMANAGEMENT/src/app.js
+++ BACKEND-CRUD/05PROJECTMANAGEMENT/src/app.js
@@ -10,7 +10,10 @@ app.use(express.static("public"))
 
 //cors configurations
 app.use(cors({
-    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173"
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET","POST","PUT","PATCH","DELETE","OPTIONS"],
+    allowedHeaders: ["Content-Type","Authorization"]
 }))
 
 app.get('/',(req,res)=>{
