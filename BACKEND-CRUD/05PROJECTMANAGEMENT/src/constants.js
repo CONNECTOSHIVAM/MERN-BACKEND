674dd24fd0ff235f3426bb58f4b3a293dbbeeeef
connectoshivam <connecttoshivam1@gmail.com>
2026-01-17 12:04:36 +0530

    add the constans of the projects

--- BACKEND-CRUD/05PROJECTMANAGEMENT/src/constants.js
+++ BACKEND-CRUD/05PROJECTMANAGEMENT/src/constants.js
@@ -1,7 +1,7 @@
 export const  UserRolesEnum = {
     ADMIN: "admin",
     PROJECT_ADMIN: "project_admin",
-    MEMBER: "member"
+    MEMBER: "member",
 };
 
 export const AvailableUserRole = Object.values(UserRolesEnum);
