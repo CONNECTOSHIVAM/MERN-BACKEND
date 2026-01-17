2969c3d23611c118a2d1b07ea882ac475733bebb
connectoshivam <connecttoshivam1@gmail.com>
2026-01-17 08:40:43 +0530

    add the cors file

--- BACKEND-CRUD/05PROJECTMANAGEMENT/src/app.js
+++ BACKEND-CRUD/05PROJECTMANAGEMENT/src/app.js
@@ -1,6 +1,17 @@
 import express from "express"
+import cors from "cors"
 
 const app = express()
+
+// basic configurations
+app.use(express.json({limit: "16kb"}));
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+
+//cors configurations
+app.use(cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173"
+}))
 
 app.get('/',(req,res)=>{
     res.send("Hi ConnectoShivam!")
