e123a664df219aa7bf461452e8008fa52cdc90be
connectoshivam <connecttoshivam1@gmail.com>
2026-01-18 18:33:54 +0530

    add the registered user functionlaties

--- BACKEND-CRUD/05PROJECTMANAGEMENT/src/controllers/auth.controllers.js
+++ BACKEND-CRUD/05PROJECTMANAGEMENT/src/controllers/auth.controllers.js
@@ -2,6 +2,7 @@ import {User} from "../models/user.models.js"
 import {ApiResponse} from "../utils/api-response.js"
 import {asyncHandler} from "../utils/async-handler.js"
 import {ApiError} from "../utils/api-error.js"
+import { emailVerificationMailgenContent, sendEmail } from "../utils/mail.js"
 
 
 const generateAccessAndRefreshTokens = async (userId) => {
@@ -43,5 +44,37 @@ const registerUser = asyncHandler(async(req, res)=>{
     //generate the temporary token
     const {unHashedToken, hashedToken, tokenExpiry} = user.generateTemporaryToken()
 
+    user.emailVerificationToken = hashedToken
+    user.emailVerificationTokenExpiry = tokenExpiry
+
+
+    await user.save({validateBeforeSave: false})
+
+    await sendEmail(
+        {
+            email: user?.email,
+            subject: "Please verify your email",
+            mailgenContent: emailVerificationMailgenContent(user.username,`${req.protocol}://${req.get("host")}/api/v1/users/verify-email/${unHashedToken}`)
+        }
+    )
+
+    const createdUser = await User.findById(user._id).select("-password -refreshToken -emailVerificationToken -emailVerificationTokenExpiry");
+
+    if(!createdUser)
+    {
+        throw new ApiError(500,"Something went wrong while registering a user")
+    }
+
+    res.status(201).json(
+        new ApiResponse(
+            200,
+            {user: createdUser},
+            "User registered successfully and verification email has been sent on your email."
+        )
+    )
+
 
 })
+
+
+export {registerUser}
