8e3e888a99de0eb4245462e65678bbdb5e4d4795
connectoshivam <connecttoshivam1@gmail.com>
2026-01-18 17:57:58 +0530

    add the token generation functionlaties

--- BACKEND-CRUD/05PROJECTMANAGEMENT/src/controllers/auth.controllers.js
+++ BACKEND-CRUD/05PROJECTMANAGEMENT/src/controllers/auth.controllers.js
@@ -3,7 +3,45 @@ import {ApiResponse} from "../utils/api-response.js"
 import {asyncHandler} from "../utils/async-handler.js"
 import {ApiError} from "../utils/api-error.js"
 
-const registerUser = asyncHandler(async(req,res)=>{
+
+const generateAccessAndRefreshTokens = async (userId) => {
+
+    try {
+         const user = await User.findById(userId);
+         const accessToken = user.generateAccessToken();
+         const refreshToken = user.generateRefreshToken();
+
+         user.refreshToken= refreshToken
+         await user.save({validateBeforeSave: false})
+         return {accessToken, refreshToken}
+    } catch (error) {
+         throw new ApiError(500, "Something went wrong while generating the access token ||",error);
+    }
+}
+
+const registerUser = asyncHandler(async(req, res)=>{
+
+  
+    const {username, email, role, password} = req.body
+
+    const existedUser = await User.findOne({
+        $or: [{username, email}]
+    })
+
+    if(existedUser)
+    {
+        throw new ApiError(409, "User with email or username is already exists",[])
+    }
+
+    const user = await User.create({
+        email,
+        password,
+        username,
+        isEmailVarified: false
+    })
+
+    //generate the temporary token
+    const {unHashedToken, hashedToken, tokenExpiry} = user.generateTemporaryToken()
 
 
 })
