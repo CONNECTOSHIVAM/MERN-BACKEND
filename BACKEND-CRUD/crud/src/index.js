44c530e3b3cd7a53852cfed0f499af8db4f0547a
connectoshivam <connecttoshivam1@gmail.com>
2026-01-20 15:45:20 +0530

    bug fix during the mongoDB connections

--- BACKEND-CRUD/crud/src/index.js
+++ BACKEND-CRUD/crud/src/index.js
@@ -11,13 +11,17 @@ const startServer = async () => {
     try {
         await connectDB()
 
+        app.get('/',(req,res)=>{
+           res.send("MongoDB Successfully Connected.")
+        })
+
         app.on("error",(error)=>{
             console.log(`Server connection error aaya ji || ${error}`);
             throw error
         })
 
         app.listen(process.env.PORT,()=>{
-            console.log(`Server is listerning at ${process.env.PORT}`);
+            console.log(`Server is listerning at ${process.env.PORT} || http://localhost:${process.env.PORT}`);
         })
         
     } catch (error) {
@@ -25,3 +29,5 @@ const startServer = async () => {
         process.exit(1);
     }
 }
+
+startServer()
