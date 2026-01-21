9d7147bf4117ca37c82754c29904c73bbe32f22c
connectoshivam <connecttoshivam1@gmail.com>
2026-01-21 13:14:02 +0530

    add the look post functionlaties

---
+++ BACKEND-CRUD/crud/src/controllers/post.controller.js
@@ -0,0 +1,54 @@
+import { Post } from "../models/post.model.js";
+
+const createPost = async (req, res) => {
+
+    try {
+        const { name, description, age } = req.body;
+
+        if (!name || !description || !age) return res.status(400).json({ message: "All fields are required." })
+
+        const post = await Post.create({
+            name,
+            description,
+            age,
+        })
+
+        res.status(201).json({
+            message: "Post created successfully ||", post
+        })
+
+    } catch (error) {
+        console.error("Server error of post creation ||",error)
+        return res.status(500).json({message: " Server is error ||"+error.message})
+    }
+
+}
+
+const lookPost = async (req, res) => {
+
+    try {
+        const posts = await Post.find()
+        res.status(200).json(posts)
+
+    } catch (error) {
+        
+        console.error("Server error of lookPost ||",error)
+        return res.status(500).json({message: " Server is error ||"+error.message})    }
+}
+
+const updatePost = async (req, res) => {
+
+    try {
+
+        const {name, description, age} = req.body;
+
+        
+
+
+        
+    } catch (error) {
+        
+    }
+}
+
+export {createPost, lookPost, updatePost}
