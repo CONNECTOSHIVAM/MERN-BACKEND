a6e7f4cfb3dbbd25c33018df4c637a31b19dc2c2
connectoshivam <connecttoshivam1@gmail.com>
2026-01-21 09:39:32 +0530

    setup the registration functionlaties

--- BACKEND-CRUD/crud/src/app.js
+++ BACKEND-CRUD/crud/src/app.js
@@ -2,6 +2,9 @@ import express from "express"
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded())
+
 //import the routes
 import userRouter from './routes/user.route.js'
 
