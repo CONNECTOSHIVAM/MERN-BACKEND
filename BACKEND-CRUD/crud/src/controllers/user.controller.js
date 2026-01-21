4b08700e4ceb1aa9182501bfdb2d326af254373b
connectoshivam <connecttoshivam1@gmail.com>
2026-01-21 12:37:24 +0530

    add the logout functionlaties

--- BACKEND-CRUD/crud/src/controllers/user.controller.js
+++ BACKEND-CRUD/crud/src/controllers/user.controller.js
@@ -1,15 +1,14 @@
 import { User } from "../models/user.model.js";
 
-const registerUser = async (req,res)=> {
+const registerUser = async (req, res) => {
 
 
     try {
-        
-        const {username, email, password} = req.body;
 
-        if(!username || !email || !password)
-        {
-            return res.status(400).json({message: "All fields are requireds"})
+        const { username, email, password } = req.body;
+
+        if (!username || !email || !password) {
+            return res.status(400).json({ message: "All fields are requireds" })
         }
 
         //check is user is exist or not.
@@ -17,8 +16,8 @@ const registerUser = async (req,res)=> {
             email: email.toLowerCase()
         })
 
-        if(existing){
-            return res.status(400).json({message: "User alredy exists."})
+        if (existing) {
+            return res.status(400).json({ message: "User alredy exists." })
         }
 
         //now if user is not exists then create a user.
@@ -27,17 +26,68 @@ const registerUser = async (req,res)=> {
             username,
             email: email.toLowerCase(),
             password,
-            loggedIn : false
+            loggedIn: false
         })
 
         res.status(201).json({
             message: "User registered successfully.",
-            user: {user: user._id,username: user.username,email: user.email}
+            user: { user: user._id, username: user.username, email: user.email }
         })
 
     } catch (error) {
-            res.status(500).json({message: "Internal server error || "+ error.message})
+        res.status(500).json({ message: "Internal server error || " + error.message })
     }
 }
 
-export {registerUser}
+const loginUser = async (req, res) => {
+
+    try {
+
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email: email.toLowerCase() })
+
+        if (!user) return res.status(404).json({ message: "Username is not found -- Please register." })
+
+        //comapre passwords
+        const isMatch = await user.comparePassword(password);
+
+        if (!isMatch) return res.status(400).json({ message: "Invalid password credentials" })
+
+        res.status(200).json({
+            message: "User Logged In successfully",
+            user: {
+                id: user._id,
+                username: user.username,
+                email: user.email
+            }
+        })
+
+
+    } catch (error) {
+
+        res.status(500).json({message: "Internal sever error - login issue || "+error.message})
+
+    }
+
+
+}
+
+const logoutUser = async (req, res) => {
+
+    try {
+        
+        const {email} = req.body;
+
+        const user = await User.findOne({email: email.toLowerCase()})
+
+        if(!user) return res.status(404).json({message: "User not found"})
+
+        res.status(200).json({message: "User logout succuessfully"})
+    } catch (error) {
+        
+        res.status(500).json({message: "Internal Server error Logout error || "+error.message})
+    }
+}
+
+export { registerUser, loginUser, logoutUser }
