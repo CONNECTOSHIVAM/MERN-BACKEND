e2fde9c13fdbfce9b0a7c4cbddf0bda75cc094e8
connectoshivam <connecttoshivam1@gmail.com>
2026-01-21 09:31:20 +0530

    modify the app js files

--- BACKEND-CRUD/crud/src/app.js
+++ BACKEND-CRUD/crud/src/app.js
@@ -2,4 +2,10 @@ import express from "express"
 
 const app = express()
 
+//import the routes
+import userRouter from './routes/user.route.js'
+
+// route declarations
+app.use("/api/v1/users",userRouter)
+
 export default app;
--- BACKEND-CRUD/crud/src/controllers/user.controller.js
+++ BACKEND-CRUD/crud/src/controllers/user.controller.js
@@ -9,9 +9,35 @@ const registerUser = async (req,res)=> {
 
         if(!username || !email || !password)
         {
-            return res.status
+            return res.status(400).json({message: "All fields are requireds"})
         }
+
+        //check is user is exist or not.
+        const existing = await User.findOne({
+            email: email.toLowerCase()
+        })
+
+        if(existing){
+            return res.status(400).json({message: "User alredy exists."})
+        }
+
+        //now if user is not exists then create a user.
+
+        const user = await User.create({
+            username,
+            email: email.toLowerCase,
+            password,
+            loggedIn : false
+        })
+
+        res.status(201).json({
+            message: "User registered successfully.",
+            user: {user: user._id,username: user.username,email: user.email}
+        })
+
     } catch (error) {
-        
+            res.status(500).json({message: "Internal server error || "+ error.message})
     }
 }
+
+export {registerUser}
