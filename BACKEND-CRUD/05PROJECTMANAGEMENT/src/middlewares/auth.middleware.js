ed6d88722ac5424d8cc59d336b42743fcdf4a495
connectoshivam <connecttoshivam1@gmail.com>
2026-01-24 21:25:37 +0530

    add the auth middleware

--- BACKEND-CRUD/05PROJECTMANAGEMENT/src/middlewares/auth.middleware.js
+++ BACKEND-CRUD/05PROJECTMANAGEMENT/src/middlewares/auth.middleware.js
@@ -1,6 +1,7 @@
 import { User } from "../models/user.models.js";
 import { asyncHandler } from "../utils/async-handler.js";
 import { ApiError } from "../utils/api-error.js";
+import jwt from "jsonwebtoken"
 
 
 export const verifyJWT = asyncHandler( async(req, res, next)=>{
@@ -9,5 +10,21 @@ export const verifyJWT = asyncHandler( async(req, res, next)=>{
     if(!token)
     {
         throw new ApiError(401,"Unauthorized request");
+    }
+
+    try {
+        const decodeToken = jwy.verify(token, process.env.ACCESS_TOKEN_SECRET)
+        const user = await User.findById(decodeToken?._id).select(
+            "-password -refreshToken -emailVerificationToken -emailVerificationExpiry",
+
+        )
+
+        if(!user){
+            throw new ApiError(401, "Invalid access Token");
+        }
+        req.user = user
+        next()
+    } catch (error) {
+        
     }
 })
