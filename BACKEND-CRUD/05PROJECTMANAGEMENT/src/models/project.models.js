9a113dc9c696e1385600f23fe6098ad6752c715b
connectoshivam <connecttoshivam1@gmail.com>
2026-01-26 12:28:24 +0530

    add the project mode file configurations

---
+++ BACKEND-CRUD/05PROJECTMANAGEMENT/src/models/project.models.js
@@ -0,0 +1,22 @@
+import mongoose,{Schema} from "mongoose";
+
+const projectSchema = new Schema({
+
+    name:{
+        type: String,
+        required: true,
+        lowecase: true,
+        trim: true,
+    },
+    description:{
+        type: String,
+        trim: true
+    },
+    createdBy:{
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    }
+},{timestamps: true})
+
+export const Project = mongoose.model("Project",projectSchema);
