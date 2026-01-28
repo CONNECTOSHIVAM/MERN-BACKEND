d2b9a1ff4f3340ccc1e0b557a326c6a726bad806
connectoshivam <connecttoshivam1@gmail.com>
2026-01-28 22:47:23 +0530

    add the create task functionlaties

--- BACKEND-CRUD/05PROJECTMANAGEMENT/src/controllers/task.controllers.js
+++ BACKEND-CRUD/05PROJECTMANAGEMENT/src/controllers/task.controllers.js
@@ -14,6 +14,39 @@ const getTasks = asyncHandler(async(req, res)=>{
 
 const createTask = asyncHandler(async(req, res)=>{
 
+    const {title, description, assignedTo, status} = req.body;
+    const {projectId} = req.params;
+
+    const project = await Project.findById(projectId)
+
+    if(!project)
+    {
+        throw new ApiError(404,"Project not found.")
+    }
+    const files = req.files || [] 
+
+    files.map((file)=>{
+        return {
+            url: `${process.env.SERVER_URL}/images/${file.originalname}/images/${file.originalname}`,
+            mimetype: file.mimetype,
+            size: file.size
+
+        }
+    })
+
+    const task = await Task.create({
+        title,
+        description,
+        project: new mongoose.Types.ObjectId(projectId),
+        assignedTo: assignedTo ? new mongoose.Types.ObjectId(assignedTo): undefined,
+        status,
+        assignedBy: new mongoose.Types.ObjectId(req.user._id),
+        attachments
+    })
+
+    return res
+            .status(200) 
+            .json(new ApiResponse(201, task, "Task created successfully."))
 })
 
 const getTasksById = asyncHandler(async(req,res)=>{
